49e2832643cc4c9d36930769a5b04c76cb08504a
projetoDeep <fonebarato56@gmail.com>
2025-05-19 10:55:51 -0300

    Update widget.js

--- widget.js
+++ widget.js
@@ -99,7 +99,7 @@ style.textContent = `
   position: fixed;
   top: 75vh; /* ← Mais abaixo da tela */
   right: auto;
-  left: 200px;
+  left: 250px;
   width: 320px;
   background: #fff;
   border-radius: 12px;
